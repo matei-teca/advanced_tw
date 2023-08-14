78e2b7cadb85523bfc067f21d7d93117e9bb875b
Matei Teca <matei.teca@gmail.com>
2023-08-14 10:58:16 +0300

    testing

--- TW_Advanced/src/main/java/com/codecool/controller/AppController.java
+++ TW_Advanced/src/main/java/com/codecool/controller/AppController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
+    // Testing
     @GetMapping("/")
     public String index(){
         return "Salut";
