eee2c1afcb1dbd32e3f8ff0f1cb619ea685924d8
Matei Teca <matei.teca@gmail.com>
2023-08-17 15:47:50 +0300

    cleanup

--- client/src/components/WelcomeCard.jsx
+++ client/src/components/WelcomeCard.jsx
@@ -92,31 +92,3 @@ export default function WelcomeCard({ searchNames }) {
         )
     )
 }
-
-      //   <div className="welcome-message">
-      //   Testing
-      //   Welcome! 
-      //   <p>
-          // Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-          // porttitor nunc. Integer condimentum, justo nec bibendum euismod,
-          // orci massa suscipit purus, in placerat lectus lacus a ligula. Donec
-          // sodales, felis nec semper sagittis, purus neque tempor nisl, ac
-          // pharetra metus tellus vel elit. Sed ultrices, nunc nec tincidunt
-          // varius, sapien nibh facilisis odio, elementum faucibus augue nisi
-          // vel massa. Ut tempus vitae enim eu congue. Vivamus sodales, orci ac
-          // condimentum aliquet, magna sapien eleifend orci, vel suscipit lectus
-          // risus pellentesque neque. Ut velit augue, commodo non scelerisque
-          // consectetur, tempus id diam. Suspendisse sed nulla in ante fermentum
-          // elementum quis non libero. 
-      //     </p>
-
-      //     <p>
-      //     Sed porttitor sodales lectus quis
-      //     lobortis. In hac habitasse platea dictumst. Aliquam massa velit,
-      //     congue in varius et, blandit a libero. In ac molestie diam. In in
-      //     dui eget sapien imperdiet condimentum. Aenean ac sem finibus,
-      //     imperdiet ipsum vel, semper arcu. Sed maximus eget metus a congue.
-      //     Praesent nec odio erat. Integer gravida non enim facilisis maximus.
-      //     Praesent nec volutpat sapien.
-      //   </p>
-      // </div> 
